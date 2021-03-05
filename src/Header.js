98a49854d34299fb106d245faef13a4618b1889f
Cody Dyar <codydyar@gmail.com>
2021-03-05 05:12:15 +0100

    added logos

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import PlayCircleOutlineTwoToneIcon from '@material-ui/icons/PlayCircleOutlineTwoTone';
+import ViewComfyRoundedIcon from '@material-ui/icons/ViewComfyRounded';
+import NotificationsNoneOutlinedIcon from '@material-ui/icons/NotificationsNoneOutlined';
 
 function Header() {
     return (
         <div className="header">
          <h1>I am a header</h1>   
          <MenuIcon />
+         <img 
+         className="header__logo"
+         src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" 
+         alt=""
+         />
+         <input type="text" />
+         <SearchIcon />
+         <PlayCircleOutlineTwoToneIcon />
+         <ViewComfyRoundedIcon />
+         <NotificationsNoneOutlinedIcon />
         </div>
     );
 }
