a5bec2068c82b640159b00e15ea0d3851ba0bf17
Cody Dyar <codydyar@gmail.com>
2021-03-05 03:39:14 +0100

    added header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from ".Header";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <h1>lets build an app</h1> */}
+
+      <Header /> 
+
+      {/* Header */}
+      {/* Sidebar */}
+      {/* RecommendedVideos */}
     </div>
   );
 }
