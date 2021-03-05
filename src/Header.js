9d24cf5091f67d99602613941e1faeb509e9216d
Cody Dyar <codydyar@gmail.com>
2021-03-05 04:12:49 +0100

    added header but correctly

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import MenuIcon from "@material-ui/icons/Menu";
 
 function Header() {
     return (
         <div className="header">
          <h1>I am a header</h1>   
+         <MenuIcon />
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
