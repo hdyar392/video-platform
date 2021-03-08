17ca773eee3e3ce50584a8877643c98261ed0aa8
Cody Dyar <codydyar@gmail.com>
2021-03-08 03:09:23 +0100

    create sidebar properties

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './App.css';
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import RecommendedVideos from "./RecommendedVideos";
-import "./App.css";
+
 
 function App() {
   return (
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import SidebarRow from './SidebarRow';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import HomeIcon from '@material-ui/icons/Home';
+import './Sidebar.css';
 
 function Sidebar() {
     return (
-        <div>
-            
+        <div className="sidebar">
+           
+          <SidebarRow Icon={HomeIcon} title="Home"/>
+          <SidebarRow Icon={WhatshotIcon} title="Trending"/>
+          <SidebarRow Icon={SubscriptionsIcon} title="Subscription"/>
+         
         </div>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
+
